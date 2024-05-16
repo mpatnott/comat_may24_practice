a6512a12a41c3f9cdd54f61cebfa64b3a62a6289
GitHub <noreply@github.com>
2024-05-16 19:06:50 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -24,8 +24,8 @@ var ptx_lunr_docs = [
   "url": "sec-worksheets.html",
   "type": "Section",
   "number": "1.2",
-  "title": "Some worksheets",
-  "body": " Some worksheets   Practicing Writing Proofs     Prove the theorem If is an odd integer and is an odd integer, then is an odd integer.       Prove the theorem For every integer , is odd if and only if is even. (Recall that this requires proving two separate implications. Identify them before writing your proofs!)      Another type of theorem that we are sometimes called upon to prove is a uniqueness theorem . uniqueness theorem  theorem uniqueness In such a case, we must prove that the thing in question exists as well as prove that there is only one such thing. The next activity walks you through the structure of such an argument.    We want to prove the theorem If such that and , then there exists a unique such that .    The first step in a uniqueness proof is to demonstrate that such a beast exists. One pattern for doing this is by coming up with a formula for the thing (here ) that we need to have exist and showing that does what it's supposed to. The process of how you come with the is scratchwork that doesn't go in the proof! . Thus, we want to start by finding a formula for so that . Use familiar algebra skills to do it. What you write up for your proof is going to look like Let . Now substituting and simplifying, we see: starting with and simplifying to get .    Next, we have to show that our is unique. To do so, you assume that both have the property required. Namely, that and . Now you use algebra to show that , which means that the you came up with originally really is unique.    What you have above likely has a lot of scratchwork in it. Rewrite things neatly to have a nice paragraph that proves the theorem.        Pick and choose amongst the following for more practice.   If is even, then is even. (The workspace after this task should be twice as long.)  If and are even, then is even. (What do you notice about your proof?)  Prove or disprove: If is odd, then there exists an integer such that . (If you decide the statement is true and prove it, can you prove that is unique?)  For every integer , if is an odd integer, then is an odd integer.     "
+  "title": "Some worksheets (example copied from Mitch)r",
+  "body": " Some worksheets (example copied from Mitch)r   Practicing Writing Proofs     Prove the theorem If is an odd integer and is an odd integer, then is an odd integer.       Prove the theorem For every integer , is odd if and only if is even. (Recall that this requires proving two separate implications. Identify them before writing your proofs!)      Another type of theorem that we are sometimes called upon to prove is a uniqueness theorem . uniqueness theorem  theorem uniqueness In such a case, we must prove that the thing in question exists as well as prove that there is only one such thing. The next activity walks you through the structure of such an argument.    We want to prove the theorem If such that and , then there exists a unique such that .    The first step in a uniqueness proof is to demonstrate that such a beast exists. One pattern for doing this is by coming up with a formula for the thing (here ) that we need to have exist and showing that does what it's supposed to. The process of how you come with the is scratchwork that doesn't go in the proof! . Thus, we want to start by finding a formula for so that . Use familiar algebra skills to do it. What you write up for your proof is going to look like Let . Now substituting and simplifying, we see: starting with and simplifying to get .    Next, we have to show that our is unique. To do so, you assume that both have the property required. Namely, that and . Now you use algebra to show that , which means that the you came up with originally really is unique.    What you have above likely has a lot of scratchwork in it. Rewrite things neatly to have a nice paragraph that proves the theorem.        Pick and choose amongst the following for more practice.   If is even, then is even.  If and are even, then is even. (What do you notice about your proof?)  Prove or disprove: If is odd, then there exists an integer such that . (If you decide the statement is true and prove it, can you prove that is unique?)  For every integer , if is an odd integer, then is an odd integer.     "
 },
 {
   "id": "sec-worksheets-2-2-1",
@@ -70,7 +70,79 @@ var ptx_lunr_docs = [
   "type": "Activity",
   "number": "1.2.4",
   "title": "",
-  "body": "  Pick and choose amongst the following for more practice.   If is even, then is even. (The workspace after this task should be twice as long.)  If and are even, then is even. (What do you notice about your proof?)  Prove or disprove: If is odd, then there exists an integer such that . (If you decide the statement is true and prove it, can you prove that is unique?)  For every integer , if is an odd integer, then is an odd integer.  "
+  "body": "  Pick and choose amongst the following for more practice.   If is even, then is even.  If and are even, then is even. (What do you notice about your proof?)  Prove or disprove: If is odd, then there exists an integer such that . (If you decide the statement is true and prove it, can you prove that is unique?)  For every integer , if is an odd integer, then is an odd integer.  "
+},
+{
+  "id": "sec-worksheet2",
+  "level": "1",
+  "url": "sec-worksheet2.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "Attempt at Own Worksheet",
+  "body": " Attempt at Own Worksheet    Here's a paragraph of text. This sentence is emphasized.  This sentence is in quotes. Here's some inline math: . Now it's time to define a term inline: An integer is even provided that there exists an integer such that .    An integer is odd provided that there exists an integer such that .      The integers are closed under multiplication.      The set of all even integers is closed under multiplication.      If is an even integer, then is even.     Observe that . Since is even, tells us that is also even.   Numbered list of my three favorite animals:  Platypus  Spider  Seastar  Unordered list of my three favorite numbers:      7 vs       Describe the differences between these two tables.       1  2  3    4  5  6         "
+},
+{
+  "id": "ws-fxn-reps-1-1",
+  "level": "2",
+  "url": "sec-worksheet2.html#ws-fxn-reps-1-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "even "
+},
+{
+  "id": "def-odd",
+  "level": "2",
+  "url": "sec-worksheet2.html#def-odd",
+  "type": "Definition",
+  "number": "1.3.1",
+  "title": "",
+  "body": "  An integer is odd provided that there exists an integer such that .   "
+},
+{
+  "id": "axiom-closure",
+  "level": "2",
+  "url": "sec-worksheet2.html#axiom-closure",
+  "type": "Axiom",
+  "number": "1.3.2",
+  "title": "",
+  "body": "  The integers are closed under multiplication.   "
+},
+{
+  "id": "lem-evensclosed",
+  "level": "2",
+  "url": "sec-worksheet2.html#lem-evensclosed",
+  "type": "Lemma",
+  "number": "1.3.3",
+  "title": "",
+  "body": "  The set of all even integers is closed under multiplication.   "
+},
+{
+  "id": "thm-evensquare",
+  "level": "2",
+  "url": "sec-worksheet2.html#thm-evensquare",
+  "type": "Theorem",
+  "number": "1.3.4",
+  "title": "",
+  "body": "  If is an even integer, then is even.   "
+},
+{
+  "id": "ws-fxn-reps-1-6",
+  "level": "2",
+  "url": "sec-worksheet2.html#ws-fxn-reps-1-6",
+  "type": "Proof",
+  "number": "[STRUCT].[NUM].1",
+  "title": "",
+  "body": " Observe that . Since is even, tells us that is also even.  "
+},
+{
+  "id": "ws-fxn-reps-2-1",
+  "level": "2",
+  "url": "sec-worksheet2.html#ws-fxn-reps-2-1",
+  "type": "Activity",
+  "number": "1.3.1",
+  "title": "",
+  "body": "Describe the differences between these two tables.       1  2  3    4  5  6      "
 },
 {
   "id": "backmatter-2",
